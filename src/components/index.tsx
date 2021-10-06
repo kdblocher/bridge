783455c722abe372530fa81d0d4c431f0f409a5d
Kyle Blocher <kyle.blocher@wagner-machine.com>
2021-10-06 14:02:32 -0500

    Screen layout fix + add github links

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -5,16 +5,19 @@ import Stats from './Stats';
 const App = () => {
   return (
     <div className="App">
-      <div style={{display: "inline-block", width: '50%'}}> 
+      <div style={{display: "inline-block", width: '50%'}}>
         <Editor />
-        ---
-        <SelectionDetails />
         ---
         <Stats />
       </div>
-      {/* <div style={{display: "inline-block", width: '50%'}}>
-        <TestHand
-      </div> */}
+      <div style={{display: "inline-block", width: '50%', float: "right"}}>
+        <div style={{display: "inline-block", float: 'right'}}>
+          <a target="_blank" rel="noreferrer" href="https://github.com/kdblocher/bridge#documentation">Docs</a>
+          &nbsp;|&nbsp;
+          <a target="_blank" rel="noreferrer" href="https://github.com/kdblocher/bridge/issues/new">Report issue</a>
+        </div>
+        <SelectionDetails />
+      </div>
     </div>
   )
 }
