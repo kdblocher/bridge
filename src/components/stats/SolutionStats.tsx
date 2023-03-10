e4bf55b3581aee3984265d908105e7198d7a0a47
Kyle D. Blocher <kdblocher@gmail.com>
2023-03-10 14:34:32 -0600

    clean up analysis start modal, move stat details to new modal

--- src/components/stats/SolutionStats.tsx
+++ src/components/stats/SolutionStats.tsx
@@ -5,18 +5,16 @@ interface SolutionStatsProps {
   stats: Stats
 }
 const SolutionStats = ({ stats }: SolutionStatsProps) => {
-  return (
-    <div>
-      <span>{stats.count !== null && <>{stats.count.toString()}</>}</span>
-      {stats.average !== null && <section>
-        <h4>Average</h4>
-        {stats.average !== null && <DoubleDummyTableView table={stats.average} />}
-      </section>}
-      {stats.stdev !== null && <section>
-        <h4>Std. Dev.</h4>
-        <DoubleDummyTableView table={stats.stdev} />
-      </section>}
-    </div>)
+  return (<>
+    {stats.average !== null && <section>
+      <h4>Average</h4>
+      {stats.average !== null && <DoubleDummyTableView table={stats.average} />}
+    </section>}
+    {stats.stdev !== null && <section>
+      <h4>Std. Dev.</h4>
+      <DoubleDummyTableView table={stats.stdev} />
+    </section>}
+  </>)
 }
 
 export default SolutionStats
