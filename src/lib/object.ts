9507848a17513f7012d5c4926827849cbd0edbc4
Kyle D. Blocher <kdblocher@gmail.com>
2023-03-12 11:47:41 -0500

    Remove unused object.map

--- src/lib/object.ts
+++ src/lib/object.ts
@@ -2,11 +2,6 @@ import ts from 'ts-toolbelt';
 
 type Key = ts.Any.Key
 
-export const map = <T, U>(obj: T, fn: (k: keyof T, v: T[keyof T], i: number) => any) =>
-  Object.fromEntries<U>(
-    (Object.entries(obj) as ([keyof T, T[keyof T]])[]).map(
-      ([k, v], i) => [k, fn(k, v, i)]))
-
 export const hasProperty = <X extends {}, Y extends PropertyKey>(obj: X, prop: Y): obj is X & Record<Y, unknown> =>
   obj.hasOwnProperty(prop)
 
